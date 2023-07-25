cc8715fce2210bdc407c19b23c3af7e18f0b2a5e
Philip Børgesen <philip.borgesen@gmail.com>
2023-07-25 05:14:08 +0200

    types: Add Placeholder type; fix misplaced docstrings

--- pyphil/types.py
+++ pyphil/types.py
@@ -1,4 +1,4 @@
-from typing import Any, Union
+from typing import Any, Union, NewType
 
 import maya.OpenMaya as om
 
@@ -6,22 +6,37 @@ __all__ = [
     "Pattern",
     "PatternLike",
     "Identifier",
+    "Placeholder",
+    "placeholder",
 ]
 
+Pattern = str
 """
 A pattern identifies one or more Maya nodes by their name (DAG path).
 It may contain wildcards. The special value "<world>" identifies the
 Maya world node.
 """
-Pattern = str
 
+PatternLike = Any
 """
 Any type that can be resolved to a Pattern by conversion via str(...).
 """
-PatternLike = Any
 
+Identifier = Union[Pattern, om.MUuid]
 """
 An identifier is either a pattern or an OpenMaya uuid object. It is
 the intention that an identifier uniquely identifies a single subject.
 """
-Identifier = Union[Pattern, om.MUuid]
+
+Placeholder = NewType("Placeholder", object)
+"""
+A placeholder marks the lack of a value, just like None.
+It can be used to denote lazily computed types Union[T, Placeholder] that
+either is some computed value of type T or the placeholder value of type
+Placeholder that represents the lack of a value.
+"""
+
+placeholder: Placeholder = Placeholder(object())
+"""
+The only value of Placeholder.
+"""
